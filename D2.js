05b02851f7d285a0a01fe6b0959366c3f7f12c46
Vincenzo De Pascale <vincenzodepascala0@gmail.com>
2022-12-05 15:05:30 +0100

    add files

--- D2.js
+++ D2.js
@@ -11,6 +11,13 @@ REGOLE
  Elenca e descrivi i principali datatype in JavaScript. Prova a spiegarli come se volessi farli comprendere a un bambino.
 */
 
+numero = 5; /* numero da 0 ad infinito senza doppi acipi " */
+stringa =
+  "stringa"; /* una serie di caratteri tra " possono essere lettere o numeri*/
+booleano = true; /* questo serve per capire se il risultato di un oprazione è "vera" o "falsa" */
+null; /* serve per indicare l'assenza di un oggetto */
+undefined; /* serve per indicare un oggetto non ancora definito, questo succede quando devi importare dati da siti esterni e lì non sono ancora caricati */
+
 /* SCRIVI QUI LA TUA RISPOSTA */
 
 /* ESERCIZIO 2
@@ -19,17 +26,28 @@ REGOLE
 
 /* SCRIVI QUI LA TUA RISPOSTA */
 
+let name = "Vincenzo";
+
 /* ESERCIZIO 3
  Scrivi il codice necessario ad effettuare un addizione (una somma) dei numeri 12 e 20.
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
 
+let number1 = 12;
+let number2 = 20;
+
+let risultato = number1 + number2;
+
+console.log("risultato ex 3 " + risultato);
+
 /* ESERCIZIO 4
  Crea una variable di nome "x" e assegna ad essa il numero 12.
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
+
+let x = 12;
 
 /* ESERCIZIO 5
   Riassegna un nuovo valore alla variabile "name" già esistente: il tuo cognome.
@@ -38,11 +56,19 @@ REGOLE
 
 /* SCRIVI QUI LA TUA RISPOSTA */
 
+name = "Vincenzo S.A.";
+const cognome = "De Pascale";
+/* cognome = "Vincenzo" */
+
+console.log(cognome);
+
 /* ESERCIZIO 6
  Esegui una sottrazione tra i numeri 4 e la variable "x" appena dichiarata (che contiene il numero 12).
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
+
+console.log(x - 4);
 
 /* ESERCIZIO 7
  Crea due variabili: "name1" e "name2". Assegna a name1 la stringa "john", e assegna a name2 la stringa "John" (con la J maiuscola!).
@@ -51,3 +77,12 @@ REGOLE
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
+
+let name1 = "john";
+let name2 = "Jhon";
+
+console.log(name1 === name2);
+
+console.log(nome1.ToLowerCase());
+console.log(nome2.ToLowerCase());
+console.log(name1 === name2);
